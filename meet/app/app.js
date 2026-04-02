62308e6efedc84186c4fefd8ec37f6290757533d
shauryachr <shauryachr@gmail.com>
2026-04-02 14:19:58 +0530

    Update app.js

--- meet/app/app.js
+++ meet/app/app.js
@@ -1,24 +1,78 @@
-<!DOCTYPE html>
-<html>
-<head>
-<title>Room</title>
-<link rel="stylesheet" href="../style.css">
-</head>
+const params = new URLSearchParams(window.location.search);
+const roomId = params.get("room");
+document.getElementById("roomId").innerText = "Room: " + roomId;
 
-<body>
+const socket = new WebSocket("wss://vasuki-meet.onrender.com");
 
-<header>
-<h3 id="roomId"></h3>
-</header>
+let localStream;
+let peers = [];
 
-<div id="videos"></div>
+async function init() {
+    localStream = await navigator.mediaDevices.getUserMedia({ video: true, audio: true });
+    addVideo(localStream, true);
 
-<div class="controls">
-<button onclick="toggleMute()">Mute</button>
-<button onclick="toggleCamera()">Camera</button>
-<button onclick="shareScreen()">Share</button>
-</div>
+    socket.send(JSON.stringify({ join: roomId }));
 
-<script src="app.js"></script>
-</body>
-</html>
+    socket.onmessage = async (msg) => {
+        const data = JSON.parse(msg.data);
+
+        if (data.offer) {
+            const pc = createPeer();
+            await pc.setRemoteDescription(data.offer);
+            const answer = await pc.createAnswer();
+            await pc.setLocalDescription(answer);
+            socket.send(JSON.stringify({ answer }));
+        }
+
+        if (data.answer) {
+            peers.forEach(pc => pc.setRemoteDescription(data.answer));
+        }
+
+        if (data.candidate) {
+            peers.forEach(pc => pc.addIceCandidate(data.candidate));
+        }
+    };
+}
+
+function createPeer() {
+    const pc = new RTCPeerConnection({
+        iceServers: [{ urls: "stun:stun.l.google.com:19302" }]
+    });
+
+    localStream.getTracks().forEach(track => pc.addTrack(track, localStream));
+
+    pc.ontrack = e => addVideo(e.streams[0]);
+
+    pc.onicecandidate = e => {
+        if (e.candidate) {
+            socket.send(JSON.stringify({ candidate: e.candidate }));
+        }
+    };
+
+    peers.push(pc);
+    return pc;
+}
+
+function addVideo(stream, muted = false) {
+    const video = document.createElement("video");
+    video.srcObject = stream;
+    video.autoplay = true;
+    video.muted = muted;
+    video.style.width = "300px";
+    document.getElementById("videos").appendChild(video);
+}
+
+function toggleMute() {
+    localStream.getAudioTracks()[0].enabled = !localStream.getAudioTracks()[0].enabled;
+}
+
+function toggleCamera() {
+    localStream.getVideoTracks()[0].enabled = !localStream.getVideoTracks()[0].enabled;
+}
+
+async function shareScreen() {
+    const screen = await navigator.mediaDevices.getDisplayMedia({ video: true });
+    addVideo(screen);
+}
+
+init();
