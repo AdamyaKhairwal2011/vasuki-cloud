60ea7369c45562c94fedf40c4b650454b3e7fa56
shauryachr <shauryachr@gmail.com>
2026-04-02 14:41:14 +0530

    Update server.js

--- meet/server/server.js
+++ meet/server/server.js
@@ -7,33 +7,29 @@ const server = http.createServer(app);
 const wss = new WebSocket.Server({ server });
 
 const rooms = {};
-const users = {};
 
 app.get("/health", (req, res) => res.send("OK"));
 
 wss.on("connection", (ws) => {
-    let room;
-
     ws.on("message", (msg) => {
         const data = JSON.parse(msg);
 
         if (data.join) {
-            room = data.join;
+            ws.room = data.join;
 
-            if (!rooms[room]) rooms[room] = [];
-            rooms[room].push(ws);
+            if (!rooms[ws.room]) rooms[ws.room] = [];
+            rooms[ws.room].push(ws);
 
-            if (!users[room]) users[room] = [];
-            users[room].push(data.user);
-
-            broadcast(room, { type: "users", users: users[room] });
+            // 🔥 notify others
+            rooms[ws.room].forEach(client => {
+                if (client !== ws) {
+                    client.send(JSON.stringify({ type: "new-user" }));
+                }
+            });
         }
 
-        if (data.type === "chat") {
-            broadcast(room, data);
-        }
-
-        rooms[room]?.forEach(client => {
+        // 🔁 relay all WebRTC signals
+        rooms[ws.room]?.forEach(client => {
             if (client !== ws && client.readyState === 1) {
                 client.send(JSON.stringify(data));
             }
@@ -41,12 +37,10 @@ wss.on("connection", (ws) => {
     });
 
     ws.on("close", () => {
-        if (rooms[room]) rooms[room] = rooms[room].filter(c => c !== ws);
+        if (ws.room && rooms[ws.room]) {
+            rooms[ws.room] = rooms[ws.room].filter(c => c !== ws);
+        }
     });
 });
-
-function broadcast(room, data) {
-    rooms[room]?.forEach(c => c.send(JSON.stringify(data)));
-}
 
 server.listen(process.env.PORT || 3000);
